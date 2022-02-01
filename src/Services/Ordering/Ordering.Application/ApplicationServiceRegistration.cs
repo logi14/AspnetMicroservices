a27f2d4f43736b2db9ac2b1c9dc0843dfffa10f4
Piotr Sidor <piotr.sid14@gmail.com>
2022-02-01 09:54:42 +0100

    Imlementation of Application Service Behaviour and Service Registration for Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection service)
+        {
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+            service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+
+            service.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandleExceptionBehavior<,>));
+            service.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return service;
+        }
+    }
+}
