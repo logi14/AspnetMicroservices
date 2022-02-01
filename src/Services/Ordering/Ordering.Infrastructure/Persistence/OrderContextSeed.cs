5bde64ebdc38a70a12873f6e319251bbcc9f8ce6
Piotr Sidor <piotr.sid14@gmail.com>
2022-02-01 13:57:16 +0100

    Implementation of Ordering.Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(
+            OrderContext orderContext,
+            ILogger<OrderContextSeed> logger)
+        {
+            if(!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", 
+                                        typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() 
+                {
+                    UserName = "swn", 
+                    FirstName = "Piotr", 
+                    LastName = "Sidor", 
+                    EmailAddress = "piotr.sid14@gmail.com", 
+                    AddressLine = "Malbork", 
+                    Country = "Poland", 
+                    TotalPrice = 350 }
+            };
+        }
+    }
+}
